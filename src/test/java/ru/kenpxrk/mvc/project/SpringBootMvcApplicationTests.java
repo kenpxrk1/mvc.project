a8c4ed686f1e03bda150b1c69cde05925605baa0
Dmitry Grishin <kenpxrkasf@gmail.com>
2024-10-03 23:21:58 +0300

    initial commit

---
+++ src/test/java/ru/kenpxrk/mvc/project/SpringBootMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kenpxrk.mvc.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
